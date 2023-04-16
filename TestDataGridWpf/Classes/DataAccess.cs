ba6675324541315c5768257b990fece876d591d9
PROGTIME-SCHOOL <117563363+PROGTIME-SCHOOL@users.noreply.github.com>
2023-04-16 19:48:05 +0300

    Initial commit

---
+++ TestDataGridWpf/Classes/DataAccess.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TestDataGridWpf.Models;
+
+namespace TestDataGridWpf.Classes
+{
+    public static class DataAccess
+    {
+        public static List<User> GetUsers()
+        {
+            List<User> users = new List<User>()
+            {
+                new User() { Id = 1, Name = "User 1"},
+                new User() { Id = 2, Name = "User 2"},
+                new User() { Id = 3, Name = "User 3"},
+                new User() { Id = 4, Name = "User 4"},
+                new User() { Id = 5, Name = "User 5"},
+            };
+
+            return users;
+        }
+    }
+}
